433a1ea29547cb3fb39d7ce193b6145fc2409a6b
Anil Krishna Narayana Swamy <ANarayanaSwamy@trcsolutions.com>
2026-06-12 20:25:59 +0530

    Removed DevExpress

--- Search-Widget/Controllers/SampleDataController.cs
+++ Search-Widget/Controllers/SampleDataController.cs
@@ -1,22 +1,22 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using Search_Widget.Models;
-using DevExtreme.AspNet.Data;
-using DevExtreme.AspNet.Mvc;
-using Microsoft.AspNetCore.Mvc;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net;
+//using System.Net.Http;
+//using Search_Widget.Models;
+//using DevExtreme.AspNet.Data;
+//using DevExtreme.AspNet.Mvc;
+//using Microsoft.AspNetCore.Mvc;
 
-namespace Search_Widget.Controllers {
+//namespace Search_Widget.Controllers {
 
-    [Route("api/[controller]")]
-    public class SampleDataController : Controller {
+//    [Route("api/[controller]")]
+//    public class SampleDataController : Controller {
 
-        [HttpGet]
-        public object Get(DataSourceLoadOptions loadOptions) {
-            return DataSourceLoader.Load(SampleData.Orders, loadOptions);
-        }
+//        [HttpGet]
+//        public object Get(DataSourceLoadOptions loadOptions) {
+//            return DataSourceLoader.Load(SampleData.Orders, loadOptions);
+//        }
 
-    }
-}
+//    }
+//}
